0a617c560b419f57873e2538b459ffa68a623149
Ikhsan Syahrizal <ikhsan.syahrizal@spesolution.com>
2024-05-28 19:31:07 +0700

    [ADD] spinner option

--- app/src/main/java/com/example/c_ask/feature/ui/directionAssist/FragmentDirectionAssitant.kt
+++ app/src/main/java/com/example/c_ask/feature/ui/directionAssist/FragmentDirectionAssitant.kt
@@ -1,11 +1,14 @@
 package com.example.c_ask.feature.ui.directionAssist
 
+import android.widget.ArrayAdapter
+import com.example.c_ask.R
 import com.example.c_ask.base.BaseFragment
 import com.example.c_ask.databinding.FragmentDirectionAssitantBinding
 
 class FragmentDirectionAssitant : BaseFragment<FragmentDirectionAssitantBinding>() {
     override fun init() {
-
     }
+
+
 
 }
